8051501a35ffb0dea84a5c37e38a45be9febf399
junho85 <junho85@gmail.com>
2021-05-04 20:45:03 +0900

    Arrays 배열 다루기

---
+++ src/chapter5/ArraysEx.java
@@ -0,0 +1,18 @@
+package chapter5;
+
+public class ArraysEx {
+
+    /*
+    배열의 비교와 출력  -equals(), toString()
+    toString 배열의 모든 요소를 문자열로 편하게 출력 할 수 있다. 일차원 배열에서만 사용 가능. 다차원 배열에서는 deepToString()을 사용하여야 한다.
+    equals()는 두 배열에 저장된 모든 요소를 비교하여 같으면 true, 다르면 false를 반환
+    일차원 배열에서만 사용가능. 다차원 배열에서 비교는 deepEquals()를 사용
+
+    배열의 복사
+    copyOf는 배열 전체를 , copyOfRange()는 배열의 일부를 복사해서 새로운 배열을 만들어 반환. 지정된 범위의 끝은 포함 되지 읺는다.
+     */
+
+
+
+
+}
